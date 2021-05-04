7c281e7033c79c389f25979d32a54bea83d13848
douglas254 <dobara@kabarak.ac.ke>
2021-05-04 11:42:59 +0300

    For loop in php and function in javascript

---
+++ Advanced Internet Programming/Javascript/Function in JavaScript.js
@@ -0,0 +1,11 @@
+// A JavaScript function is a block of code designed to perform a particular task.
+// A JavaScript function is executed when "something" invokes it (calls it).
+// Why Functions?
+// You can reuse code: Define the code once, and use it many times.
+
+// You can use the same code many times with different arguments, to produce different results.
+
+function multiplication(p1, p2){
+    console.log(p1*p2)
+}
+multiplication(7,2) // Calling function
---
+++ Advanced Internet Programming/hypertext preprocessor(PHP)/forloop.php
@@ -0,0 +1,13 @@
+<?php
+# The for loop - Loops through a block of code a specified number of times.
+//  Using PHP, write a for loop that that prints numbers 2 to 10 
+
+for ($x=2;$x <= 10; $x++){
+    echo"$x <br>";
+}
+
+// $x = 0; - Initialize the loop counter ($x), and set the start value to 0
+// $x <= 10; - Continue the loop as long as $x is less than or equal to 10
+// $x++ - Increase the loop counter value by 1 for each iteration
+
+?>
